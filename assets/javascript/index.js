c0301be08cd528e0bd0430303357609239d25434
MiroDev20 <mirodev20@gmail.com>
2026-08-18 12:22:44 -0500

    chore(index.js): add DOM elements mapping in items object

---
+++ assets/javascript/index.js
@@ -0,0 +1,9 @@
+const items = {
+    audio   : document.querySelector('[data-audio]'),
+    bar     : document.querySelector('[data-progression]'),
+    repeat  : document.querySelector('[data-action="repeat"]'),
+    back    : document.querySelector('[data-action="previous"]'),
+    play    : document.querySelector('[data-action="play"]'),
+    next    : document.querySelector('[data-action="next"]'),
+    shuffle : document.querySelector('[data-action="shuffle"]')
+}
