7f2fc7ae2eab98e0db81af9e291074f2f08bedc7
MiroDev20 <mirodev20@gmail.com>
2026-08-31 19:26:52 -0500

    chore: add Ame album tracks to music library
    Add three tracks from the 2026 album 'Ame' by ビクター MKII:
    - Everything is Cold
    - Promise
    - Saints

---
+++ assets/javascript/audio-data.js
@@ -0,0 +1,29 @@
+export const library = [
+    {
+        backgroundImage: 'none',
+        posterURL: '../images/ame.jpg',
+        title: 'Everything is Cold',
+        album: 'Ame',
+        year: 2026,
+        artist: 'ビクター MKII',
+        audioPath : '../audios/everything-is-cold.mpeg'
+    },
+    {
+        backgroundImage: 'none',
+        posterURL: '../images/ame.jpg',
+        title: 'Promise',
+        album: 'Ame',
+        year: 2026,
+        artist: 'ビクター MKII',
+        audioPath : '../audios/promise.mpeg'
+    },
+    {
+        backgroundImage: 'none',
+        posterURL: '../images/ame.jpg',
+        title: 'Saints',
+        album: 'Ame',
+        year: 2026,
+        artist: 'ビクター MKII',
+        audioPath : '../audios/saints.mpeg'
+    }
+]
