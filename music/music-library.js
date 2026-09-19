523030c5cf5ea2171253d8b145b275c4ae02c227
MiroDev20 <mirodev20@gmail.com>
2026-09-19 12:08:44 -0500

    chore: reorganize JSON structure for audio files

---
+++ music/music-library.js
@@ -0,0 +1,26 @@
+export const MusicLibrary = [
+    {
+        id: "ame",
+        title: "Ame",
+        artist: "ビクター MKII",
+        year: 2026,
+        cover: "./assets/images/ame.jpeg",
+        tracks: [
+            {
+                id: "everything-is-cold",
+                title: "Everything is Cold",
+                audio: "./assets/audios/everything-is-cold.mpeg"
+            },
+            {
+                id: "promise",
+                title: "Promise",
+                audio: "./assets/audios/promise.mpeg"
+            },
+            {
+                id: "saints",
+                title: "Saints",
+                audio: "./assets/audios/saints.mpeg"
+            }
+        ]
+    },
+];
